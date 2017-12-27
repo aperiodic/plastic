9d7a1f8d35d15567ae2f9ad367849effb057c0df
aperiodic <dlp@aperiodic.org>
2017-12-27 10:58:45 -0800

    Publishing events to core.async chan

--- examples/cypress/examples/hello_world.cljs
+++ examples/cypress/examples/hello_world.cljs
@@ -1,4 +1,21 @@
 (ns cypress.examples.hello-world
   (:require [cypress.core :as cyp]))
 
-(.log js/console "Hello world!")
+(enable-console-print!)
+
+(defn sandbox-node
+  []
+  (.getElementById js/document "sandbox"))
+
+(defn clear-sandbox!
+  []
+  (let [sandbox (sandbox-node)
+        new-sandbox (.cloneNode sandbox)]
+    (.replaceChild (.-parentNode sandbox) new-sandbox sandbox)))
+
+(defn start!
+  []
+  (cyp/init (sandbox-node)))
+
+(clear-sandbox!)
+(start!)
--- src/cypress/core.cljs
+++ src/cypress/core.cljs
@@ -1,1 +1,34 @@
-(ns cypress.core)
+(ns cypress.core
+  (:require-macros [cljs.core.async.macros :refer [go go-loop]])
+  (:require [clojure.core.async :refer [>! <! chan take! put!]]))
+
+(defn publish-event!
+  [chan kind page-x page-y]
+  (put! chan {:kind kind, :page-x page-x, :page-y page-y}))
+
+(defn event-handler
+  [events-chan kind]
+  (fn [e]
+    (publish-event! events-chan kind (.-pageX e) (.-pageY e))))
+
+(defn event-processor
+  [incoming]
+  (go-loop []
+    (when-let [{:keys [kind page-x page-y]} (<! incoming)]
+      (.log js/console "got a" (name kind) "event at (" page-x "," page-y ")")
+      (recur))))
+
+(def ^:private kind->type
+  {:mouse-down "mousedown"
+   :mouse-up "mouseup"
+   :mouse-move "mousemove"})
+
+(defn init
+  [dom-event-root]
+  (let [ui-events (chan 16)]
+    (event-processor ui-events)
+    (doseq [[event-kind dom-type] kind->type]
+      (.addEventListener dom-event-root
+        dom-type
+        (event-handler ui-events event-kind)
+        #js {:passive false}))))
