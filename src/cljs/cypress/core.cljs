d13244f20d503fcbcc4e4799514cb5e001d087fa
Dan Lidral-Porter <dlidral-porter@cylance.com>
2018-03-23 01:29:58 -0700

    Support more events, only listen for what's used
    Add in the rest of the DOM & touch events that I could think of / find
    in the MDN documentation.
    
    In order to avoid registering a bunch of useless listeners that don't
    actually trigger transitions in the application's UI state machine, only
    register handlers for events that are used in some transition of the
    state machine.

--- src/cljs/cypress/core.cljs
+++ src/cljs/cypress/core.cljs
@@ -1,6 +1,7 @@
 (ns cypress.core
   (:require-macros [cljs.core.async.macros :refer [go go-loop]])
   (:require [clojure.core.async :refer [>! <! chan take! put!]]
+            [clojure.set :as set :refer [subset?]]
             [cypress.state-machine :as sm]))
 
 (defn publish-event!
@@ -13,14 +14,32 @@
     (publish-event! events-chan kind e)))
 
 (def ^:private event-kind->name
-  {:mouse-down "mousedown"
+  {:click "click"
+   :double-click "dblclick"
+   :focus-in "focusin"
+   :focus-out "focusout"
+   :key-down "keydown"
+   :key-up "keyup"
+   :key-press "keypress"
+   :input "input"
+   :mouse-down "mousedown"
+   :mouse-move "mousemove"
+   :mouse-out "mouseout"
+   :mouse-over "mouseover"
    :mouse-up "mouseup"
-   :mouse-move "mousemove"})
+   :select "select"
+   :touch-cancel "touchcancel"
+   :touch-end "touchend"
+   :touch-move "touchmove"
+   :touch-start "touchstart"
+   :wheel "wheel"})
 
-(def event-name->kind
+(def ^:private event-name->kind
   (into {} (for [[k n] event-kind->name]
              [n k])))
 
+(def supported-event-kinds (set (keys event-kind->name)))
+(def supported-event-names (set (vals event-kind->name)))
 
 (defn event-processor
   [state-machine dom-events !state]
@@ -37,15 +56,29 @@
 
 (defn init!
   [dom-event-root ui-state-machine !app-state]
+  ;; ensure the state machine is well-formed
   (when-not (sm/valid? ui-state-machine)
     (throw
       (js/TypeError.
         (str "Invalid state machine: " (sm/validation-error ui-state-machine)))))
+  ;; ensure the state machine only uses supported events
+  (when-not (subset? (sm/events ui-state-machine) supported-event-kinds)
+    (let [unsupported (set/difference (sm/events ui-state-machine)
+                                      supported-event-kinds)
+          plural? (> (count unsupported) 1)]
+      (throw
+        (js/TypeError.
+          (str "Unsupported event "
+               (if plural? "types" "type") " "
+               (if plural? (seq unsupported) (first unsupported)))))))
   (let [ui-events (chan 16)]
+    ;; start the event loop
     (event-processor ui-state-machine ui-events !app-state)
-    ;; register a handler for every supported event type
-    (doseq [[event-kind dom-event-name] event-kind->name]
-      (.addEventListener dom-event-root
-        dom-event-name
-        (event-handler ui-events event-kind)
-        #js {:passive false}))))
+    ;; register a handler publishing to the loop's channel for every kind of
+    ;; event that triggers any transition in the ui-state-machine
+    (doseq [event-kind (sm/events ui-state-machine)]
+      (let [dom-event-name (event-kind->name event-kind)]
+        (.addEventListener dom-event-root
+          dom-event-name
+          (event-handler ui-events event-kind)
+          #js {:passive false})))))
