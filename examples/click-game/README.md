d71dab03b682175cfdde28ef9ebd1fe2e085e475
aperiodic <dlp@aperiodic.org>
2018-08-05 21:45:39 -0700

    Replace trivial dispatch example with a game
    The game is quite simple: click on all the dots to win. The hit
    detection shows the kind of situation where the dynamic dispatches
    really comes in handy.
    
    The game is a fully standalone project that you can go into and start
    hacking on right away if you'd like.

---
+++ examples/click-game/README.md
@@ -0,0 +1,15 @@
+# dynamic-transitions
+
+This is your new cypress- & om-based webapp.
+Right now, it tracks mouse clicks; the rest is up to you.
+
+## Usage
+
+Start it by running `lein figwheel`.
+This will start a local webserver on port 3449 that live-reloads your application when you make code changes.
+
+## License
+
+Copyright © FIXME
+
+Distributed under the FIXME License
---
+++ examples/click-game/project.clj
@@ -0,0 +1,29 @@
+(defproject cypress/click-game "0.1.0-SNAPSHOT"
+  :description "A simple click-the-dots game to show how to add logic to state machine transitions."
+  :url "https://example.com"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.946"]
+                 [org.clojure/core.async "0.3.465"]
+                 [org.omcljs/om "1.0.0-beta1"]
+                 [cypress "0.2.0"]]
+
+  :source-paths ["src/cljs"]
+  :plugins [[lein-cljsbuild "1.1.7"]
+            [lein-figwheel "0.5.14"]]
+
+  :figwheel {:css-dirs ["resources/public"]}
+  :cljsbuild {
+    :builds [{
+      :id "dev"
+      :source-paths ["src/cljs"]
+      :figwheel true
+      :compiler {
+        :main "cypress.examples.click-game"
+        :asset-path "js/out"
+        :output-dir "resources/public/js/out"
+        :output-to "resources/public/js/game.js"
+        :optimizations :none
+        :pretty-print true}}]})
